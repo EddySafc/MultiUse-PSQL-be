50e6c02d4dc594c8724d4d4f23a68ca5d56db810
EddySafc <eddy@eddy-730QED>
2023-03-20 11:21:21 +0000

    stuff for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,21 @@
 const { Pool } = require("pg");
-const ENV = "development";
 
+const ENV = process.env.NODE_ENV || "development";
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
+//...
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+      }
+    : {};
+module.exports = new Pool(config);
+//...
 module.exports = new Pool();
