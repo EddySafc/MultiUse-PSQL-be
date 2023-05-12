dd71717d3555db3301a97cb7c1f1990b374aaef5
EddySafc <eddy@eddy-730QED>
2023-05-12 13:26:57 +0100

    end points description

--- controller.js
+++ controller.js
@@ -18,13 +18,43 @@ const {
 } = require("./model");
 
 exports.getEndpointsDescription = (req, res, next) => {
-  fetchEndpointsDescription()
-    .then((result) => {
-      res.send({ result });
-    })
-    .catch((err) => {
-      next(err);
-    });
+  res.send({
+    "GET /api": {
+      description:
+        "serves up a json representation of all the available endpoints of the api",
+      exampleResponse: {
+        endpoints: [{ This: "File" }],
+      },
+    },
+    "GET /api/dailys": {
+      description: "serves an array of all daily to dos",
+      queries: [],
+      exampleResponse: {
+        topics: [{ todo_id: 1, todo: "Run" }],
+      },
+    },
+    "GET /api/weeklys": {
+      description: "serves an array of all weekly to dos",
+      queries: [],
+      exampleResponse: {
+        topics: [{ todo_id: 7, todo: "Hoover" }],
+      },
+    },
+    "GET /api/monthlys": {
+      description: "serves an array of all monthly to dos",
+      queries: [],
+      exampleResponse: {
+        topics: [{ todo_id: 9, todo: "Mow Lawn" }],
+      },
+    },
+  });
+  // fetchEndpointsDescription()
+  //   .then((result) => {
+  //     res.send({ result });
+  //   })
+  //   .catch((err) => {
+  //     next(err);
+  //   });
 };
 
 exports.getDailys = (req, res, next) => {
